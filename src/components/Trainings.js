95cbfddf42a089c9501080024b91a434dda7dd83
Austin Randall <randall@Randall.local>
2018-01-15 13:39:52 -0700

    training component css and layout is done

--- src/components/Trainings.js
+++ src/components/Trainings.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import './css/Trainings.css'
 
 export default class Trainings extends Component {
     render() {
         return (
-            <div><Header />Trainings</div>
+            <div className='trainings-main'>
+                <Header />
+                <div className='trainings-content'>
+                    <div className='trainings-category'>
+                    Training Category One</div>
+                    <div className='trainings-category'>
+                    Training Category Two</div>
+                    <div className='trainings-category'>
+                    Training Category Three</div>
+                    <div className='trainings-category'>
+                    Training Category Four</div>
+                    <div className='trainings-category'>
+                    Training Category Five</div>
+                    <div className='trainings-category'>
+                    Training Category Six</div>
+                </div>
+            </div>
         );
     }
 }
